96ec1792e08b784787613b9a6b3630fd91340b75
Laura Alias <laura.alias.email@gmail.com>
2018-12-12 19:39:40 +0000

    Actual Test

--- public/js/helpers/url.test.js
+++ public/js/helpers/url.test.js
@@ -2,6 +2,10 @@ import {getHashParams} from "./url"
 
 describe("tests for url.js", () => {
 	test("testing getHashParams", () => {
-	
-	})
+	const token = "BQAnRUDRCSyudlnkr3HP5KwhaLmjsHj2dPXewQJptkRhXKdBt5cOOAeDBUXzx5qv_e71P99XQIL7YdNzSVj7DL0-2rr4Htj361LwQVmt7coIGwawfrhwpob9i5Jt3th7oJB05DOd7a2CZ1EPxqwl7_y2y5GddFSdFqcgqgRG_5LWK-HlqMf3FQ"
+	const state = "Q3ZaB8LSSQSdpO9G"
+	const params = getHashParams();
+	expect(params.access_token).toBe(token);
+	expect(params.state).toBe(state);
+	});
 })
